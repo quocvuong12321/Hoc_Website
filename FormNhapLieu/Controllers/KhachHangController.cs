39b715eecfb973c375a07b122912ddfcbffaccf7
Nguyen Vuong <nguyenquocvuong12321@gmail.com>
2024-07-13 00:37:56 +0700

    Tao project Form nhap lieu

---
+++ FormNhapLieu/Controllers/KhachHangController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FormNhapLieu.Models;
+
+namespace FormNhapLieu.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        public ActionResult DanhSach()
+        {
+            return View(DanhSachKhachHang.dsKhachHang);
+        }
+
+        public ActionResult ThemMoi()
+        {
+            return View(new KhachHang() { Id = 0 });
+        }
+        [HttpPost]
+        public ActionResult ThemMoi(KhachHang model)
+        {
+            if(model.Id == 0)
+            {
+                ModelState.AddModelError("", "Id phải > 0");
+                return View(model);
+            }
+
+            DanhSachKhachHang.dsKhachHang.Add(model);
+
+            return RedirectToAction("DanhSach");
+        }
+
+    }
+}
---
+++ FormNhapLieu/Models/KhachHang.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormNhapLieu.Models
+{
+    public class KhachHang
+    {
+        private int id;
+        string tenKH;
+        string soDT;
+        string diaChiKH;
+        string email;
+        string gioiTinh;
+
+        public KhachHang(int id, string tenKH, string soDT, string diaChiKH, string email, string gioiTinh)
+        {
+            Id = id;
+            TenKH = tenKH;
+            SoDT = soDT;
+            DiaChiKH = diaChiKH;
+            Email = email;
+            GioiTinh = gioiTinh;
+        }
+
+        public KhachHang() { }
+
+        public int Id { get => id; set => id = value; }
+        public string TenKH { get => tenKH; set => tenKH = value; }
+        public string SoDT { get => soDT; set => soDT = value; }
+        public string DiaChiKH { get => diaChiKH; set => diaChiKH = value; }
+        public string Email { get => email; set => email = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+
+    }
+}
