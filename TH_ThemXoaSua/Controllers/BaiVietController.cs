e7e4b6ea181921accf92c841a58ee9a8a06045d5
Nguyen Vuong <nguyenquocvuong12321@gmail.com>
2024-07-19 23:46:02 +0700

    Thuc hanh them xoa sua va su dung CkEditor

---
+++ TH_ThemXoaSua/Controllers/BaiVietController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH_ThemXoaSua.Models;
+namespace TH_ThemXoaSua.Controllers
+{
+    public class BaiVietController : Controller
+    {
+        // GET: BaiViet
+        QuanLyBaiVietEntities db = new QuanLyBaiVietEntities();
+        public ActionResult DanhSach()
+        {
+            
+            return View(db.BaiViets.ToList());
+        }
+
+        public ActionResult ThemMoi()
+        {
+
+            return View(new BaiViet());
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ThemMoi(BaiViet model, HttpPostedFileBase fname)
+        {
+            if (string.IsNullOrEmpty(model.TENBAIVIET))
+            {
+                ModelState.AddModelError("", "Thiếu tên bài viết");
+                return View(model);
+            }
+
+            string rootFolder = Server.MapPath("/img/");
+            string path = rootFolder + fname.FileName;
+            fname.SaveAs(path);
+            model.HINHANH = fname.FileName;
+            db.BaiViets.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("DanhSach");
+        }
+
+        //Các bước nhúng ckEditor
+        //1. Tải bộ plugin vào project (dùng install-package CkEditor
+        //2. Kéo thả file JS vào layout
+        //3. Thay đổi input nội dung thành textarea, có đặt id cho input
+        //4. Viết lệnh js cho textarea
+        //5. Lưu dữ liệu: - thêm kiểm tra html  cho action lưu dữ liệu
+
+
+        public ActionResult ChinhSua(int id)
+        {
+            return View(db.BaiViets.FirstOrDefault(t => t.ID == id));
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult ChinhSua(BaiViet model)
+        {
+            BaiViet bv = db.BaiViets.FirstOrDefault(t => t.ID == model.ID);
+            if (string.IsNullOrEmpty(model.TENBAIVIET))
+            {
+                ModelState.AddModelError("", "Thiếu tên bài viết");
+                return View(model);
+            }
+
+            bv.NOIDUNG = model.NOIDUNG;
+            db.SaveChanges();
+            return RedirectToAction("DanhSach");
+        }
+
+
+        public ActionResult Xoa(int id)
+        {
+            db.BaiViets.Remove(db.BaiViets.Find(id));
+            db.SaveChanges();
+            return RedirectToAction("DanhSach");
+        }
+
+        public ActionResult ChiTiet(int id)
+        {
+            return View(db.BaiViets.Find(id));
+        }
+    }
+}
---
+++ TH_ThemXoaSua/Controllers/LoaiBaiVietController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH_ThemXoaSua.Models;
+namespace TH_ThemXoaSua.Controllers
+{
+    public class LoaiBaiVietController : Controller
+    {
+        // GET: LoaiBaiViet
+        QuanLyBaiVietEntities db = new QuanLyBaiVietEntities();
+        public ActionResult DanhSach()
+        {
+            List<LoaiBaiViet> lstLBV = db.LoaiBaiViets.ToList();
+            return View(lstLBV);
+        }
+
+
+        public ActionResult ThemMoi() {
+            return View();
+        } 
+
+        [HttpPost]
+        public ActionResult ThemMoi(LoaiBaiViet model)
+        {
+            if (string.IsNullOrEmpty(model.TENLOAI))
+            {
+                ModelState.AddModelError("", "Thiếu thông tin tên loại");
+                return View(model);
+            }
+            try {
+                db.LoaiBaiViets.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("DanhSach");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(model);
+            }
+        }
+
+        public ActionResult CapNhat(int id)
+        {
+            LoaiBaiViet lbv = db.LoaiBaiViets.Find(id);
+            return View(lbv);
+        }
+
+        [HttpPost]
+        public ActionResult CapNhat(LoaiBaiViet model)
+        {
+            LoaiBaiViet lbv = db.LoaiBaiViets.FirstOrDefault(t=>t.ID==model.ID);
+            if (string.IsNullOrEmpty(model.TENLOAI))
+            {
+                ModelState.AddModelError("", "Thiếu thông tin tên loại");
+                return View(model);
+            }
+            try
+            {
+                lbv.TENLOAI = model.TENLOAI;
+                db.SaveChanges();
+                return RedirectToAction("DanhSach");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(model);
+            }
+        }
+        public ActionResult Xoa(int id)
+        {
+            db.LoaiBaiViets.Remove(db.LoaiBaiViets.FirstOrDefault(t=>t.ID==id));
+            db.SaveChanges();
+            return RedirectToAction("DanhSach");
+        }
+    }
+}
